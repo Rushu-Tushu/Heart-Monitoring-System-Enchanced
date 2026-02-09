05e43e1be36ffba27bf82edbb93cfc958cff5875
Rushikesh Sapkal <rushikeshsapkal930@gmail.com>
2026-02-09 23:59:59 +0530

    Organizing code and documentating it

--- backend/models/Reading.js
+++ backend/models/Reading.js
@@ -1,27 +1,39 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
+// Each document here represents ONE heart reading
 const ReadingSchema = new mongoose.Schema({
+
+  // Reference to the user who owns this reading
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     required: true,
   },
+
+  // Current heart rate
   heartRate: {
     type: Number,
     required: true,
   },
+
+  // Average heart rate
   avgHeartRate: {
     type: Number,
     required: true,
   },
+
+  // Oxygen level
   spo2: {
     type: Number,
     required: true,
   },
+
+  // Time when reading was taken
   timestamp: {
     type: Date,
     default: Date.now,
   },
-});
+})
 
-export default mongoose.model("Reading", ReadingSchema);
+// Export Reading model
+export default mongoose.model("Reading", ReadingSchema)
